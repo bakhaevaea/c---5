65584b0bde3ef9763904fec516be1dbf99d2e156
Евгения <bakhaeva@gmail.com>
2022-09-04 13:24:33 +0300

    добавлена задача 36

--- Program.cs
+++ Program.cs
@@ -21,15 +21,28 @@ class MyNewClass
         System.Console.WriteLine($"Количество четных элементов {countNum}" );
         
     }
+
+    public static void MyTask36()
+    {
+        //Задайте одномерный массив, заполненный случайными числами. 
+        //Найдите сумму элементов, стоящих на нечетных позициях
+        System.Console.WriteLine("Задайте размер массива ");
+        int Len = int.Parse(Console.ReadLine());
+        int [] MyArr = new int [Len];
+        int Summa = 0;
+        for (int i = 0; i < Len; i++)
+        {
+            MyArr[i] = new Random().Next(-20,21);
+            if (i%2 == 1)  Summa += MyArr[i];
+        }
+
+        System.Console.WriteLine(string.Join("  ", MyArr ));
+        System.Console.WriteLine($"Сумма элементов на нечетных позициях {Summa}" );   
+    }
     static void Main()
     {
-       // MyTask32();
-
-        // bool nInA=MyTask33();
-        // Console.WriteLine( nInA ? "Да" : "Нет");
-
-        MyTask34();
-       
+       // MyTask34();
+        MyTask36();
     }
     
 
