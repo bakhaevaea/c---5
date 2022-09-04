3f5f1c93597ef5be9819828460308f7664e442f6
Евгения <bakhaeva@gmail.com>
2022-09-04 13:08:59 +0300

    задача 34

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class MyNewClass
+{
+
+    public static void MyTask34()
+    {
+        //Задайте массив, заполненный случайными положительными трехзначными числами. 
+        //Напишите программу, которая покажет количество четных чисел в массиве
+        System.Console.WriteLine("Задайте размер массива ");
+        int Len = int.Parse(Console.ReadLine());
+        int [] MyArr = new int [Len];
+        int countNum = 0;
+        for (int i = 0; i < Len; i++)
+        {
+            MyArr[i] = new Random().Next(100,1000);
+            if (MyArr[i]%2 == 0)  countNum++;
+        }
+
+        System.Console.WriteLine(string.Join("  ", MyArr ));
+        System.Console.WriteLine($"Количество четных элементов {countNum}" );
+        
+    }
+    static void Main()
+    {
+       // MyTask32();
+
+        // bool nInA=MyTask33();
+        // Console.WriteLine( nInA ? "Да" : "Нет");
+
+        MyTask34();
+       
+    }
+    
+
+}
